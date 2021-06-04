7de0977b43166339c2a22eae56a773872e8ac57f
Astami Vishwakarma <astami@shipperstack.com>
2021-06-04 14:08:31 +0530

    added tailwind CSS to svelte next
    Signed-off-by: Astami Vishwakarma <astami@shipperstack.com>

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,8 @@
+<main>
+    <slot />
+</main>
+<style global lang="postcss">
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,22 @@
 import preprocess from 'svelte-preprocess';
+import ssr from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [
+		preprocess({
+		  defaults: {
+			style: 'postcss'
+		  },
+		  postcss: true
+		})
+	],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: ssr(),
 		target: '#svelte'
 	}
 };
