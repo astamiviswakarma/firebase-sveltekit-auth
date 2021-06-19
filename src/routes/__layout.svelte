4568937c3e0b479010eb1e83a8acac8af3cb36f0
Astami Vishwakarma <astami@shipperstack.com>
2021-06-19 11:28:29 +0530

    using poppins instead of Roboto
    Signed-off-by: Astami Vishwakarma <astami@shipperstack.com>

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -38,7 +38,21 @@
   });
 </script>
 
-<nav>
+<header class="mdc-top-app-bar">
+  <div class="mdc-top-app-bar__row">
+    <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+      <button class="mdc-icon-button material-icons mdc-top-app-bar__navigation-icon--unbounded">menu</button>
+      <span class="mdc-top-app-bar__title">San Francisco, CA</span>
+    </section>
+    <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end">
+      <button class="mdc-icon-button material-icons mdc-top-app-bar__action-item--unbounded" aria-label="Download">file_download</button>
+      <!-- <button class="mdc-icon-button material-icons mdc-top-app-bar__action-item--unbounded" aria-label="Print this page">print</button> -->
+      <!-- <button class="mdc-icon-button material-icons mdc-top-app-bar__action-item--unbounded" aria-label="Bookmark this page">bookmark</button> -->
+    </section>
+  </div>
+</header>
+
+<nav class="pt-16">
   <a href="/">Home</a> |
   {#if $session && !$session.user}
   <a href="/login">Login</a>
@@ -55,6 +69,14 @@
 <style global lang="postcss">
   @import 'material-components-web/dist/material-components-web';
   @import 'material-icons/iconfont/material-icons.css';
+  @import '@fontsource/poppins';
+
+  html {
+    font-family: "Poppins";
+    --mdc-typography-font-family: "Poppins";
+    --mdc-theme-primary: brown;
+  }
+
   @tailwind base;
   @tailwind components;
   @tailwind utilities;
